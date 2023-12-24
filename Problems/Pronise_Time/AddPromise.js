5f0bfa5e7999ede257c42509b971e2484ab765c6
Sagar <sagarpuniyani@gmail.com>
2023-12-24 16:16:39 +0530

    Add function to add two promises

---
+++ Problems/Pronise_Time/AddPromise.js
@@ -0,0 +1,15 @@
+/**
+ * @param {Promise} promise1
+ * @param {Promise} promise2
+ * @return {Promise}
+ */
+var addTwoPromises = async function(promise1, promise2) {
+    let value1 = await promise1;
+    let value2 = await promise2;
+    return value1 + value2;
+};
+
+
+
+addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+        .then(console.log); // 4
