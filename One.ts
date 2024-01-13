30cab4198bb35ab87bff8d159703304110c44a1c
Sagar <sagarpuniyani@gmail.com>
2024-01-13 22:51:54 +0530

    Fix variable declaration and update array type

--- One.ts
+++ One.ts
@@ -1,11 +1,11 @@
 var a: number = 76567;
 
 let b: number; // Declare b before using it
-b = 90;
+b = 90
 var c = a+b;
 
 var d: string = "SagarPuniyani";
-var e = "hkjdhh";
+var e: string = "hkjdhh";
 
 var t : boolean = true ;
 console.log(" t = " , t );
@@ -14,7 +14,7 @@ console.log(" t = " , t );
 // let num = BigInt(7878798);
 var arr = [12 , 34, 45 , "hioijoi" , true , false];
 
-var w1 : (number | string )[] = [ 12 ,345  , 56 , " tkjgro" ,"boitjgpoj"]
+var w1 : (string | number  )[] = [ 12 ,345  , 56 , " tkjgro" ,"boitjgpoj" , 455 , "hjbjkj"]
 
 
 const obj = {
