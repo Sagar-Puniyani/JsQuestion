477456c679aa8b0e49eaf0a12b28b1ec2afd0001
Sagar <sagarpuniyani@gmail.com>
2024-02-04 13:33:06 +0530

    Add file IO and timers in Working.js

--- js/Working.js
+++ js/Working.js
@@ -9,3 +9,15 @@ setImmediate(() => {
     console.log('Hello from the Immediate 1 ');
 })
 
+fs.readFile('sample.c++' , 'utf-8' , ()=>{
+    console.log('IO polling finish');
+
+    setTimeout(() => {
+        console.log('Hello from Timer 2');
+    }, 0);
+    setTimeout(()=>{
+        console.log('Hello from Timer 3 ');
+    } , 5*100)
+})
+
+console.log("hello From Top Level ");
