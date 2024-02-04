6f565bd5e3212c083f3fc1c7f1ac9c8a6e0fac7d
Sagar <sagarpuniyani@gmail.com>
2024-02-04 19:14:21 +0530

    Add UV_THREADPOOL_SIZE and crypto tasks

--- js/Working.js
+++ js/Working.js
@@ -1,5 +1,9 @@
 const fs = require('fs')
+const crypto = require('crypto')
 
+process.env.UV_THREADPOOL_SIZE = 10; 
+
+const start = Date.now()
 setTimeout(() => {
     console.log('Hello from Timer 1 ');
 }, 0);
@@ -9,7 +13,7 @@ setImmediate(() => {
     console.log('Hello from the Immediate 1 ');
 })
 
-fs.readFile('sample.c++' , 'utf-8' , ()=>{
+fs.readFile('6six.js' , 'utf-8' , ()=>{
     console.log('IO polling finish');
 
     setTimeout(() => {
@@ -18,6 +22,36 @@ fs.readFile('sample.c++' , 'utf-8' , ()=>{
     setTimeout(()=>{
         console.log('Hello from Timer 3 ');
     } , 5*100)
+
+    setImmediate(() => {
+        console.log('Hello from the Immediate 2 ');
+    })
+
+    // Crpto Task 
+    crypto.pbkdf2('password1' , 'salt1' , 100000 , 1024 , 'sha512' , ()=>{
+        console.log(`${Date.now() - start} ms `,'password 1 Complete ');
+    } )
+
+    crypto.pbkdf2('password2' , 'salt1' , 100000 , 1024 , 'sha512' , ()=>{
+        console.log(`${Date.now() - start} ms `,'password 2 Complete ');
+    } )
+
+    crypto.pbkdf2('password3' , 'salt1' , 100000 , 1024 , 'sha512' , ()=>{
+        console.log(`${Date.now() - start} ms `,'password 3 Complete ');
+    } )
+
+    crypto.pbkdf2('password4' , 'salt1' , 100000 , 1024 , 'sha512' , ()=>{
+        console.log(`${Date.now() - start} ms `,'password 4 Complete ');
+    } )
+
+    crypto.pbkdf2('password5' , 'salt1' , 100000 , 1024 , 'sha512' , ()=>{
+        console.log(`${Date.now() - start} ms `,'password 5 Complete ');
+    } )
+
+    crypto.pbkdf2('password6' , 'salt1' , 100000 , 1024 , 'sha512' , ()=>{
+        console.log(`${Date.now() - start} ms `,'password 6 Complete ');
+    } )
+
 })
 
 console.log("hello From Top Level ");
