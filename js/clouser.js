0c1bae33d47bbeb07ff310f423e19420fcab8545
Sagar <sagarpuniyani@gmail.com>
2024-02-08 23:08:02 +0530

    Add additional counter functionality to createCounter function

--- js/clouser.js
+++ js/clouser.js
@@ -11,3 +11,17 @@ const counter = createCounter();
 console.log(counter());  // Output: 1
 console.log(counter());  // Output: 2
 console.log(counter());  // Output: 3
+
+function createCounter() {
+    let count = 0;
+    return function() {
+        count += 1;
+        return count;
+    };
+}
+
+const counter1 = createCounter();
+
+console.log(counter1());  // Output: 1
+console.log(counter1());  // Output: 2
+console.log(counter1());  // Output: 3
